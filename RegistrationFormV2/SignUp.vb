30b10bd668c329933cbb04e8f1ef997a6abbc819
rrvrflws <reignreigns04@gmail.com>
2025-08-02 14:35:24 +0800

    changes picture box color to gray.

--- RegistrationFormV2/SignUp.vb
+++ RegistrationFormV2/SignUp.vb
@@ -93,4 +93,8 @@ Public Class SignUp
             End If
         End Using
     End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
 End Class
